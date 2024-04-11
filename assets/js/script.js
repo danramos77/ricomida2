f5b4004f8c1cc660fbb0c3dc53ce1e363a483d1d
Yerko Cruz <ycruzramos@gmail.com>
2024-04-10 22:13:10 -0400

    Commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//$("btnleft").hover(
+//    function(){
+        
+})
+//
+//var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//  return new bootstrap.Tooltip(tooltipTriggerEl)
+})//
+
+//var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//  return new bootstrap.Tooltip(tooltipTriggerEl)
+//})//
+
+$(document).ready(function() {
+    $("#mensajes").click(function() {
+      alert("El correo fue enviado correctamente...");
+    });
+  });
